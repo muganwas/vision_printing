1b2d6e327da2ca5f4e0298c121d69c7f6d405544
muganwas <stevenmuganwa@live.com>
2017-04-02 14:43:58 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<script async='async' src='https://www.googletagservices.com/tag/js/gpt.js'></script>
+
+<script>
+  var googletag = googletag || {};
+  googletag.cmd = googletag.cmd || [];
+</script>
+
+<script>
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/16375024/VPrinting_930x180_Front_dtop', [930, 180], 'div-gpt-ad-1489405880098-0').addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
+</script>
+
+	<meta charset="utf-8">
+	<title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, user-scalable=yes"/>
+	<meta name="description" content="Printing template">
+	<meta name="author" content="Netbase">
+    <link rel="stylesheet" type="text/css" href="css/style-main.css"> 
+	 <!--add header.php--> 
+<?php include "header.php";?>
+	<!--Main index : Begin-->
+<main class="main index">
+		<!--Home slider : Begin-->
+<section class="home-slidershow">
+			<link href="css/style-main.css" rel="stylesheet" type="text/css">
+<div class="slide-show">
+				<div class="vt-slideshow">
+					<ul>
+                    
+                    <li class="slide1" data-transition="random" ><img src="images/slider/home/bg_slider_4.jpg" alt=""/>
+							<div class="tp-caption lfr" data-x="right"  data-hoffset="-80" data-y="170" data-start="800" data-speed="2000" data-endspeed="300"><span class="style1"><span class="textcolor">Bringing Print to Life</span></span></div> 
+							<div class="tp-caption lfb" data-x="right"  data-hoffset="-15" data-y="225" data-start="800" data-speed="2000" data-endspeed="300"
+							>
+								<span class="style2">
+                                    Our promise is to provide the highest-quality products<br>
+                                    and services possible in a timely fashion and at a competitive price.<br>
+                                    We promise to listen to you and help you achieve your business goals.<br>
+                                    We will always be there with honest, expert advice and quick, <br>
+                                    friendly customer service.<br>
+                                    Our goal is to not only meet, but exceed your expectations.   
+							  </span>
+							</div>
+							
+					  </li>
+                    <li class="slide1" data-transition="random" ><img src="images/slider/home/bg_slider_0.jpg" alt="" />
+							<div class="tp-caption lfr" data-x="right"  data-hoffset="-136" data-y="170" data-start="800" data-speed="2000" data-endspeed="300"><span class="style1"><span class="textcolor">Our Promise</span> & Goal</span></div> 
+							<div class="tp-caption lfb" data-x="right"  data-hoffset="-15" data-y="225" data-start="800" data-speed="2000" data-endspeed="300"
+							>
+								<span class="style2">
+                                    Our promise is to provide the highest-quality products<br>
+                                    and services possible in a timely fashion and at a competitive price.<br>
+                                    We promise to listen to you and help you achieve your business goals.<br>
+                                    We will always be there with honest, expert advice and quick, <br>
+                                    friendly customer service.<br>
+                                    Our goal is to not only meet, but exceed your expectations.   
+							  </span>
+							</div>
+							
+					  </li>
+						<li class="slide1" data-transition="random" ><img src="images/slider/home/bg_slider_1.jpg" alt="" />
+							<div class="tp-caption lfr" data-x="left"  data-hoffset="" data-y="170" data-start="800" data-speed="2000" data-endspeed="300"><span class="style1"><span class="textcolor">Best Quality</span> Machinery</span></div> 
+							<div class="tp-caption lfb" data-x="left"  data-hoffset="" data-y="225" data-start="800" data-speed="2000" data-endspeed="300"
+							>
+								<span class="style2">
+									As a result of top range machinery – CTP<br>
+                                    Pre-press system with very minimal dot loss/gain to your repro quality,<br> 
+                                    computerised inking system on the presses that ensure even inking and <br>
+                                    enhanced proof matching capabilities, quality checking systems<br>
+                                    at finishing stage that ensures the deliveries you get are top quality.<br> 
+								</span>
+							</div>
+							
+						</li>
+						<li class="slide2" data-transition="random" ><img src="images/slider/home/bg_slider_2.jpg" alt="" />
+							<div class="tp-caption lfr" data-x="left"  data-hoffset="" data-y="170" data-start="800" data-speed="2000" data-endspeed="300"><span class="style1"><span class="textcolor">Quality Graphic</span> & Design</span></div> 
+							<div class="tp-caption lfb" data-x="left"  data-hoffset="" data-y="225" data-start="800" data-speed="2000" data-endspeed="300">
+								<span class="style2">
+									Quality graphic & design is imperative for your brand image<br> 
+                                    as it acts as the initial connection between your business<br>
+                                    and your potential customers. It should make a statement<br>
+                                    about who you are as a company and should set you apart<br>
+                                    from your competitors.
+								</span>
+							</div>
+							 
+						</li>
+					  <blockquote>&nbsp;</blockquote>
+                    </ul> 
+				</div>
+		</div>
+</section>
+		<!--Home Category : Begin-->
+<section class="home-category">
+			<div class="container1">
+ <div class="row">          
+<div class="block-title-w">
+						<h2 class="block-title">about us</h2>
+                        <span class="icon-title">
+							<span></span>
+							<i class="fa fa-star"></i>
+						</span> 
+              <p class="aboutz">Vision printing has been in the commercial printing business for 30years and offers a wide range of printing solutions that include security printing, digital printing services, offset printing for both sheet fed and web as well as finishing.</p>
+              <div class="row">
+                <div class="aboutus_d">
+                  <div class="in_d"><img src="images/icons/competitive_pricing.fw.png"><div class="about_text">Competitive Pricing </div></div>
+                  <div class="in_d"><img src="images/icons/quality_printing.fw.png"><div class="about_text">Impeccable Quality </div></div>
+                  <div class="in_d"><img src="images/icons/fast_delivery.fw.png"><div class="about_textr">Fast Delivery<br/><br/></div></div>
+                  
+                </div>
+                <div class="catalog"><a href="images/NVPProfile 2016 mail.pdf">Download  Our Catalogue</a></div>
+              </div>          
+</div>
+	</div>
+		</section> 
+        <!-- unseen but needed-->
+        <section class="home-capabititie">
+			<div class="container">
+				<div class="row">
+					<div class="block-title-w">
+						<h2 class="block-title">our capabilities</h2> 
+						<span class="sub-title">Mauris sollicitudin dui quisOrci euismod mattis Integer auctor Aenean id turpis eu est iaculis suscipit.</span>
+						<span class="icon-title">
+							<span></span>
+							<i class="fa fa-star"></i>
+						</span> 
+					</div>
+					<div class="block-capabititie-w">
+						<div class="block-capabititie col-md-3 col-sm-3 col-xs-12">
+							<div class="block-mydoughnut" id="myDoughnut"> </div>
+							<h2 class="title">huge quantity</h2>
+							<div class="decs">
+							<p>Mauris sollicitudin dui quisOrci euismod mattis Integer auctor Aenean id turpis eu est iaculis suscipit.</p>
+							</div>
+						</div>
+						<div class="block-capabititie col-md-3 col-sm-3 col-xs-12">
+							<div class="block-mydoughnut" id="myDoughnut2"> </div>
+							<h2 class="title">on Time</h2>
+							<div class="decs">
+							<p>Mauris sollicitudin dui quisOrci euismod mattis Integer auctor Aenean id turpis eu est iaculis suscipit.</p>
+							</div>
+						</div>
+						<div class="block-capabititie col-md-3 col-sm-3 col-xs-12">
+							<div class="block-mydoughnut" id="myDoughnut3"> </div>
+							<h2 class="title">True Color</h2>
+							<div class="decs">
+							<p>Mauris sollicitudin dui quisOrci euismod mattis Integer auctor Aenean id turpis eu est iaculis suscipit.</p>
+							</div>
+						</div>
+						<div class="block-capabititie col-md-3 col-sm-3 col-xs-12">
+							<div class="block-mydoughnut" id="myDoughnut4"> </div>
+							<h2 class="title">fast Delivery</h2>
+							<div class="decs">
+							<p>Mauris sollicitudin dui quisOrci euismod mattis Integer auctor Aenean id turpis eu est iaculis suscipit.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!--Home out recent : Begin -->
+<section class="home-out-recent">
+			<div class="container">
+<div class="row">
+					<div class="block-title-w">
+						<h2 class="block-title">SOME OF OUR CLIENTS</h2> 
+						<span class="icon-title">
+							<span></span>
+							<i class="fa fa-star"></i>
+						</span>
+					</div>
+                    
+					<div class="tab-content">
+					  <?php include_once "clients.php" ?>
+				</div>
+             </div>
+		</div>
+      </div>
+ 
+</section>
+</main>
+<?php include "footer.php";?>
+	<div id="sitebodyoverlay"></div>
+	<?php include "mobile_menu.php"; ?>
+
+	<!--Add js lib-->
+	<script type="text/javascript" src="js/jquery/jquery-1.11.3.min.js"></script> 
+	<script type="text/javascript" src="js/bootstrap.min.js"></script> 
+    <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+    <script type="text/javascript" src="js/Chart.js"></script> 
+    <script type="text/javascript" src="js/doughnutit.js"></script>
+  <!--   <script type="text/javascript" src="js/jquery.subscribe-better.js"></script> -->
+	<script type="text/javascript" src="js/slideshow/jquery.themepunch.revolution.js"></script>   
+	<script type="text/javascript" src="js/slideshow/jquery.themepunch.plugins.min.js"></script>  
+    <script type="text/javascript" src="js/theme-home.js"></script>  
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  	<![endif]-->
+</body>
+</html>
